09b90267ef77fb45ca2e81492bdf1cfd3a3c6e91
yyeonkim <seyeon.dev@gmail.com>
2022-01-03 15:12:44 +0900

    Theme 사용하기

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="teal">
-        <Emoji>😍</Emoji>
-      </Box>
+      <Title>Hello Theme</Title>
     </Wrapper>
   );
 }
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
-  height: 100%;
+  height: 100vh;
   justify-content: center;
   align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    transform: rotate(360deg);
-      border-radius: 40px;
-  }
-  100% {
-    transform: rotate(0deg);
-      border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1.5s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 50px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 export default App;
