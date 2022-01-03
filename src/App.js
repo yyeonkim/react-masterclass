573563ce9b1d91ab174530d581f5d07d9e990fbc
yyeonkim <seyeon.dev@gmail.com>
2022-01-03 12:45:46 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="teal">
-        <span>😍</span>
-      </Box>
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle bgColor="skyblue" />
     </Wrapper>
   );
 }
@@ -14,38 +14,14 @@ const Wrapper = styled.div`
   display: flex;
 `;
 
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 50px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
 const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1.5s linear infinite;
-  span {
-    font-size: 36px;
-    &:hover {
-      font-size: 50px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 export default App;
