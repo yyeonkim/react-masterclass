a2b96a08b084423fd93788de8f6eac1b8483a004
yyeonkim <seyeon.dev@gmail.com>
2022-01-03 13:46:44 +0900

    Pseudo Selectors
    styled-components 태그로 선택해주기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="teal" />
-      <Box bgColor="tomato" />
-      <Circle bgColor="skyblue" />
+      <Box bgColor="teal">
+        <Emoji>😍</Emoji>
+      </Box>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+  50% {
+    transform: rotate(360deg);
+      border-radius: 40px;
+  }
+  100% {
+    transform: rotate(0deg);
+      border-radius: 0;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
 `;
 
 const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1.5s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 export default App;
