9d3aec31b3783c8c75af8c2e06f1e34b32044f79
yyeonkim <seyeon.dev@gmail.com>
2022-01-04 15:12:57 +0900

    Forms practice
    TS 연습하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value}
+    } = event;
+    setValue(value);
+  }
+
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input value={value} type="text" placeholder="username" onChange={onChange}  />
+        <button >Log in</button>
+      </form>
     </div>
   );
 }
