c62ed0b3463bb3b77a46dc1c03699396b6f3b588
yyeonkim <seyeon.dev@gmail.com>
2022-01-19 12:10:47 +0900

    Feat: Add filter to show only selected category

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,40 +1,33 @@
-import { useRecoilValue } from "recoil";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 
-import { toDoSelector } from "../atoms";
+import { categoryState, toDoSelector } from "../atoms";
 import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
 
 export default function ToDoList() {
-  const [toDos, doing, done] = useRecoilValue(toDoSelector);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState); // Selected category
+
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">TO DO</option>
+        <option value="DOING">DOING</option>
+        <option value="DONE">DONE</option>
+      </select>
+
       <ToDoForm />
-
-      <h2>TO DO</h2>
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-
-      <hr />
-      <h2>DOING</h2>
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-
-      <hr />
-      <h2>DONE</h2>
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
