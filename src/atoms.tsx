09e07b5fd523f89b4d5253fa20dd42046f1c3b9b
yyeonkim <seyeon.dev@gmail.com>
2022-01-19 11:41:22 +0900

    Feat: Use selectors to sort toDos

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   id: number;
@@ -9,4 +9,16 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((todo) => todo.category === "TO_DO"),
+      toDos.filter((todo) => todo.category === "DOING"),
+      toDos.filter((todo) => todo.category === "DONE"),
+    ];
+  },
 });
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,37 @@
 import { useRecoilValue } from "recoil";
 
-import { toDoState, IToDo } from "../atoms";
+import { toDoSelector } from "../atoms";
 import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
 
 export default function ToDoList() {
-  const toDos = useRecoilValue<IToDo[]>(toDoState);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <ToDoForm />
+
+      <h2>TO DO</h2>
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+
+      <hr />
+      <h2>DOING</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+
+      <hr />
+      <h2>DONE</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
