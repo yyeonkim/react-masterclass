a1e033f0baca0cab538291424bbfac3677735438
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-18 11:21:09 +0900

    Add movement across boards

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,32 @@ export default function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
   const onDragEnd = (result: DropResult) => {
-    const { draggableId, destination, source } = result;
+    const { destination, source } = result;
+    // Same index
+    if (!destination) return;
 
-    // Move in same board
+    // Move in the same board
     if (destination.droppableId === source.droppableId) {
-      // Same index
-      if (!destination.index) return;
-
       const boardCopy = [...toDos[source.droppableId]];
       const [removed] = boardCopy.splice(source.index, 1);
 
       boardCopy.splice(destination.index, 0, removed);
       setToDos({ ...toDos, [source.droppableId]: boardCopy });
+    } else {
+      // Move across boards
+      const sourceBoard = [...toDos[source.droppableId]];
+      const destinationBoard = [...toDos[destination.droppableId]];
+
+      const [removed] = sourceBoard.splice(source.index, 1);
+      destinationBoard.splice(destination.index, 0, removed);
+
+      setToDos({
+        ...toDos,
+        [source.droppableId]: sourceBoard,
+        [destination.droppableId]: destinationBoard,
+      });
     }
   };
-
-  // 1) Delete item on sourceIndex
-  // 2) Put back the item on destinationIndex
-  // const reorder = (
-  //   list: string[],
-  //   sourceIndex: number,
-  //   destinationIndex: number
-  // ) => {};
 
   return (
     <Wrapper>
@@ -51,7 +55,7 @@ export default function App() {
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 80rem;
+  max-width: 100rem;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -62,7 +66,7 @@ const Wrapper = styled.div`
 
 const Boards = styled.div`
   width: 100%;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 2rem;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
