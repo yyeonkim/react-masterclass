1679a95c066db31767c6f34d68895379d4e13553
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-18 12:55:09 +0900

    Add animation

--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -10,8 +10,9 @@ interface IDraggableCardProps {
 function DraggableCard({ toDo, index }: IDraggableCardProps) {
   return (
     <Draggable draggableId={toDo} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
@@ -23,11 +24,21 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
   );
 }
 
-const Card = styled.div`
-  background-color: ${(props) => props.theme.cardColor};
-  border-radius: 0.5rem;
-  padding: 0.5rem;
+const Card = styled.div<{ isDragging: boolean }>`
+  display: flex;
+  align-items: center;
   margin-bottom: 1rem;
+  background-color: ${(props) =>
+    props.isDragging ? "#ffb703" : props.theme.cardColor};
+  border-radius: 0.5rem;
+  padding: ${(props) => (props.isDragging ? "4rem 2rem" : "2rem 2rem")};
+  box-shadow: ${(props) =>
+    props.isDragging
+      ? "1.7px 1.6px 3.3px rgba(0, 0, 0, 0.04), 4.3px 4.2px 8.3px rgba(0, 0, 0, 0.058), 8.9px 8.5px 17px rgba(0, 0, 0, 0.072), 18.3px 17.5px 35px rgba(0, 0, 0, 0.09), 50px 48px 96px rgba(0, 0, 0, 0.13) "
+      : 0};
+
+  transition: all 0.1s ease;
+  transition: padding 2s ease-in;
 `;
 
 export default React.memo(DraggableCard);
