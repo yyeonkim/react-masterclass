c7588c5b5c1af8a0c260e69e1c452a5aa7b1b796
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-23 11:18:52 +0900

    Add gestures: whileHover, whileTap

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,14 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const boxVariants = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.1,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const circleVariants = {
-  start: { opacity: 0, y: 20 },
-  end: { opacity: 1, y: 0 },
+  hover: { scale: 1.2, rotate: 90 },
+  tap: { borderRadius: "50%", scale: 0.8 },
 };
 
 export default function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box variants={boxVariants} whileHover="hover" whileTap="tap" />
     </Wrapper>
   );
 }
@@ -46,9 +26,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 20rem;
   height: 20rem;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
