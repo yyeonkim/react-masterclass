a2380fbbc5cad3ea6408f3318d45c052448fc54f
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-23 11:31:25 +0900

    Add drag

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import { motion } from "framer-motion";
 const boxVariants = {
   hover: { scale: 1.2, rotate: 90 },
   tap: { borderRadius: "50%", scale: 0.8 },
+  drag: { backgroundColor: "rgb(255, 221, 89)", transition: { duration: 0.1 } },
 };
 
 export default function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="tap" />
+      <Box
+        variants={boxVariants}
+        drag
+        whileDrag="drag"
+        whileHover="hover"
+        whileTap="tap"
+      />
     </Wrapper>
   );
 }
@@ -28,14 +35,5 @@ const Box = styled(motion.div)`
   height: 20rem;
   background-color: rgba(255, 255, 255);
   border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 50%;
-  place-self: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
