dda7feb4c316997dcca888a4ba7bb6f02a351afc
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-23 11:06:34 +0900

    Use variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
+const boxVariants = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const circleVariants = {
+  start: { opacity: 0, y: 20 },
+  end: { opacity: 1, y: 0 },
+};
+
 export default function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", damping: 10 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
@@ -25,7 +46,18 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 20rem;
   height: 20rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
   background-color: white;
-  border-radius: 15px;
+  height: 70px;
+  width: 70px;
+  border-radius: 50%;
+  place-self: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
