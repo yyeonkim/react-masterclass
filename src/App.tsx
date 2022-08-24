3d6d6f7e0b5e4c0330cce9867a1e2a3c699d18c3
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-24 14:59:41 +0900

    feat: Change scale by scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 export default function App() {
   const x = useMotionValue(0);
-  const scaleByX = useTransform(x, [-800, 0, 800], [0.1, 1, 2]);
+  const rotateZByX = useTransform(x, [-800, 800], [-360, 360]);
+  const gradientByX = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(70deg, rgb(107, 90, 255), rgb(97, 220, 251))",
+      "linear-gradient(70deg, rgb(255, 90, 205), rgb(251, 218, 97))",
+      "linear-gradient(70deg, rgb(255, 90, 90), rgb(255, 139, 62))",
+    ]
+  );
+  const { scrollY, scrollYProgress } = useScroll();
+  const scaleByScroll = useTransform(scrollYProgress, [0, 1], [1, 2]);
 
   return (
-    <Wrapper>
-      <Box style={{ x, scale: scaleByX }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradientByX }}>
+      <Box
+        style={{ x, rotateZ: rotateZByX, scale: scaleByScroll }}
+        drag="x"
+        dragSnapToOrigin
+      />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(to bottom left, #ff5acd, #fbda61);
+  background: linear-gradient(70deg, rgb(255, 90, 205), rgb(251, 218, 97));
 `;
 
 const Box = styled(motion.div)`
